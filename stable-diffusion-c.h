0d80582825605b8b7b36ea225fda3e4d57dab62c
Cyberhan123 <255542417@qq.com>
2023-11-11 10:36:38 +0800

    refactor c api

--- stable-diffusion-c.h
+++ stable-diffusion-c.h
@@ -26,66 +26,44 @@ extern "C"
 #include <stdint.h>
 #include <string.h>
 
-	struct sd_txt2img_options;
-
-	struct sd_img2img_options;
-
-	// These methods are used in binding in other languages,golang, python,etc.
-
-	//==============================sd_txt2img_options===============================
-	STABLE_DIFFUSION_API sd_txt2img_options* new_sd_txt2img_options();
-
-	STABLE_DIFFUSION_API void set_txt2img_prompt(sd_txt2img_options* opt, const char* prompt);
-
-	STABLE_DIFFUSION_API void set_txt2img_negative_prompt(sd_txt2img_options* opt, const char* negative_prompt);
-
-	STABLE_DIFFUSION_API void set_txt2img_cfg_scale(sd_txt2img_options* opt, float cfg_scale);
-
-	STABLE_DIFFUSION_API void set_txt2img_size(sd_txt2img_options* opt, int width, int height);
-
-	STABLE_DIFFUSION_API void set_txt2img_sample_method(sd_txt2img_options* opt, const char* sample_method);
-
-	STABLE_DIFFUSION_API void set_txt2img_sample_steps(sd_txt2img_options* opt, int sample_steps);
-
-	STABLE_DIFFUSION_API void set_txt2img_seed(sd_txt2img_options* opt, int64_t seed);
-	//================================================================================
-
-	//==============================sd_img2img_options===============================
-	STABLE_DIFFUSION_API sd_img2img_options* new_sd_img2img_options();
-
-	STABLE_DIFFUSION_API void set_img2img_init_img(sd_img2img_options* opt, const uint8_t* init_img, int64_t size);
-
-	STABLE_DIFFUSION_API void set_img2img_prompt(sd_img2img_options* opt, const char* prompt);
-
-	STABLE_DIFFUSION_API void set_img2img_negative_prompt(sd_img2img_options* opt, const char* negative_prompt);
-
-	STABLE_DIFFUSION_API void set_img2img_cfg_scale(sd_img2img_options* opt, float cfg_scale);
-
-	STABLE_DIFFUSION_API void set_img2img_size(sd_img2img_options* opt, int width, int height);
-
-	STABLE_DIFFUSION_API void set_img2img_sample_method(sd_img2img_options* opt, const char* sample_method);
-
-	STABLE_DIFFUSION_API void set_img2img_sample_steps(sd_img2img_options* opt, int sample_steps);
-
-	STABLE_DIFFUSION_API void set_img2img_strength(sd_img2img_options* opt, float strength);
-
-	STABLE_DIFFUSION_API void set_img2img_seed(sd_img2img_options* opt, int64_t seed);
-	//================================================================================
 
 	STABLE_DIFFUSION_API void* create_stable_diffusion(int n_threads,
 		bool vae_decode_only,
 		bool free_params_immediately,
-		const char* rng_type);
+		enum RNGType rng_type);
 
 	STABLE_DIFFUSION_API void destroy_stable_diffusion(void* sd);
 
-	STABLE_DIFFUSION_API bool load_from_file(void* sd, const char* file_path, const char* schedule);
+	STABLE_DIFFUSION_API bool load_from_file(void* sd, const char* file_path,enum Schedule schedule);
 
-	STABLE_DIFFUSION_API uint8_t* txt2img(void* sd, sd_txt2img_options* opt, int64_t* output_size);
+	STABLE_DIFFUSION_API uint8_t* txt2img(
+	    void* sd,
+	    const char* prompt;
+        const char* negative_prompt;
+	    float cfg_scale,
+        int width,
+        int height,
+        enum SampleMethod sample_method,
+        int sample_steps,
+        int64_t seed,
+        int64_t* output_size);
 
-	STABLE_DIFFUSION_API uint8_t* img2img(void* sd, sd_img2img_options* opt, int64_t* output_size);
+	STABLE_DIFFUSION_API uint8_t* img2img(
+        void* sd,
+        const uint8_t* init_img,
+        const int64_t init_img_size,
+        const char* prompt,
+        const char* negative_prompt,
+        float cfg_scale,
+        int width,
+        int height,
+        enum SampleMethod sample_method,
+        int sample_steps,
+        float strength,
+        int64_t seed
+        int64_t* output_size);
 
-	STABLE_DIFFUSION_API void set_stable_diffusion_log_level(const char* level);
+	STABLE_DIFFUSION_API void set_stable_diffusion_log_level(enum SDLogLevel level);
 
 	STABLE_DIFFUSION_API const char* get_stable_diffusion_system_info();
 
