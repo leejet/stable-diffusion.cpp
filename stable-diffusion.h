a27a92c74dab6f707ffb3e10c37eb8fb8190be38
Cyberhan123 <255542417@qq.com>
2023-11-11 10:39:36 +0800

    rollback stable-diffusion.h

--- stable-diffusion.h
+++ stable-diffusion.h
@@ -1,85 +1,71 @@
 #ifndef __STABLE_DIFFUSION_H__
 #define __STABLE_DIFFUSION_H__
 
-#include <assert.h>
 #include <memory>
-#include <vector>
-#include <algorithm>
-#include <cstring>
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <random>
-#include <regex>
-#include <set>
-#include <sstream>
-#include <string>
-#include <unordered_map>
 #include <vector>
 
 enum SDLogLevel {
-	DEBUG,
-	INFO,
-	WARN,
-	ERROR
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR
 };
 
 enum RNGType {
-	STD_DEFAULT_RNG,
-	CUDA_RNG
+    STD_DEFAULT_RNG,
+    CUDA_RNG
 };
 
 enum SampleMethod {
-	EULER_A,
-	EULER,
-	HEUN,
-	DPM2,
-	DPMPP2S_A,
-	DPMPP2M,
-	DPMPP2Mv2,
-	N_SAMPLE_METHODS
+    EULER_A,
+    EULER,
+    HEUN,
+    DPM2,
+    DPMPP2S_A,
+    DPMPP2M,
+    DPMPP2Mv2,
+    N_SAMPLE_METHODS
 };
 
 enum Schedule {
-	DEFAULT,
-	DISCRETE,
-	KARRAS,
-	N_SCHEDULES
+    DEFAULT,
+    DISCRETE,
+    KARRAS,
+    N_SCHEDULES
 };
 
 class StableDiffusionGGML;
 
 class StableDiffusion {
-private:
-	std::shared_ptr<StableDiffusionGGML> sd;
+   private:
+    std::shared_ptr<StableDiffusionGGML> sd;
 
-public:
-	StableDiffusion(int n_threads = -1,
-		bool vae_decode_only = false,
-		bool free_params_immediately = false,
-		RNGType rng_type = STD_DEFAULT_RNG);
-	bool load_from_file(const std::string& file_path, Schedule d = DEFAULT);
-	std::vector<uint8_t> txt2img(
-		const std::string& prompt,
-		const std::string& negative_prompt,
-		float cfg_scale,
-		int width,
-		int height,
-		SampleMethod sample_method,
-		int sample_steps,
-		int64_t seed);
-	std::vector<uint8_t> img2img(
-		const std::vector<uint8_t>& init_img,
-		const std::string& prompt,
-		const std::string& negative_prompt,
-		float cfg_scale,
-		int width,
-		int height,
-		SampleMethod sample_method,
-		int sample_steps,
-		float strength,
-		int64_t seed);
+   public:
+    StableDiffusion(int n_threads = -1,
+                    bool vae_decode_only = false,
+                    bool free_params_immediately = false,
+                    RNGType rng_type = STD_DEFAULT_RNG);
+    bool load_from_file(const std::string& file_path, Schedule d = DEFAULT);
+    std::vector<uint8_t> txt2img(
+        const std::string& prompt,
+        const std::string& negative_prompt,
+        float cfg_scale,
+        int width,
+        int height,
+        SampleMethod sample_method,
+        int sample_steps,
+        int64_t seed);
+    std::vector<uint8_t> img2img(
+        const std::vector<uint8_t>& init_img,
+        const std::string& prompt,
+        const std::string& negative_prompt,
+        float cfg_scale,
+        int width,
+        int height,
+        SampleMethod sample_method,
+        int sample_steps,
+        float strength,
+        int64_t seed);
 };
 
 void set_sd_log_level(SDLogLevel level);
