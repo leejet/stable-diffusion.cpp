1e9fd072e4aace206e5436d7f1ac50568e785eaf
Daniele <57776841+daniandtheweb@users.noreply.github.com>
2024-08-07 05:49:45 +0200

    fix typo

--- examples/cli/main.cpp
+++ examples/cli/main.cpp
@@ -203,7 +203,7 @@ void print_usage(int argc, const char* argv[]) {
     printf("  -b, --batch-count COUNT            number of images to generate.\n");
     printf("  --schedule {discrete, karras, ays} Denoiser sigma schedule (default: discrete)\n");
     printf("  --prediction {eps, v, flow}\n");
-    printf("                                     prediction type (default: \"eps\")\n");
+    printf("                                     prediction mode (default: eps)\n");
     printf("  --clip-skip N                      ignore last layers of CLIP network; 1 ignores none, 2 ignores one layer (default: -1)\n");
     printf("                                     <= 0 represents unspecified, will be 1 for SD1.x, 2 for SD2.x\n");
     printf("  --vae-tiling                       process vae in tiles to reduce memory usage\n");
