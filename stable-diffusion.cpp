c61cfecb8c7547735b030db844ee3d1d64457c76
Stéphane du Hamel <stephduh@live.fr>
2025-04-02 16:14:47 +0200

    conditionner: make text encoders optional for Flux

--- stable-diffusion.cpp
+++ stable-diffusion.cpp
@@ -326,6 +326,7 @@ public:
             clip_backend   = backend;
             bool use_t5xxl = false;
             if (sd_version_is_dit(version)) {
+                // TODO: check if t5 is actually loaded?
                 use_t5xxl = true;
             }
             if (!clip_on_cpu && !ggml_backend_is_cpu(backend) && use_t5xxl) {
