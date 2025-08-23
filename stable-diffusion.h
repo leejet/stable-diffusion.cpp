786d950f45b523f89c65c7e0963beaa15fc66429
Green Sky <Green-Sky@users.noreply.github.com>
2025-08-23 12:01:53 +0200

    update ggml and adjust for new type

--- stable-diffusion.h
+++ stable-diffusion.h
@@ -96,12 +96,13 @@ enum sd_type_t {
     // SD_TYPE_Q4_0_4_4 = 31, support has been removed from gguf files
     // SD_TYPE_Q4_0_4_8 = 32,
     // SD_TYPE_Q4_0_8_8 = 33,
-    SD_TYPE_TQ1_0 = 34,
-    SD_TYPE_TQ2_0 = 35,
+    SD_TYPE_TQ1_0   = 34,
+    SD_TYPE_TQ2_0   = 35,
     // SD_TYPE_IQ4_NL_4_4 = 36,
     // SD_TYPE_IQ4_NL_4_8 = 37,
     // SD_TYPE_IQ4_NL_8_8 = 38,
-    SD_TYPE_COUNT = 39,
+    SD_TYPE_MXFP4 = 39, // MXFP4 (1 block)
+    SD_TYPE_COUNT   = 40,
 };
 
 enum sd_log_level_t {
