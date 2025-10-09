f1db617452f9cf4f69ccf893a4309d81ae6b766c
Wagner Bruna <wbruna@users.noreply.github.com>
2025-10-09 19:17:35 -0300

    fix encoding VAE tiling for Qwen Image

--- stable-diffusion.cpp
+++ stable-diffusion.cpp
@@ -1440,10 +1440,19 @@ public:
         if (vae_tiling_params.enabled && !encode_video) {
             // TODO wan2.2 vae support?
             int C = sd_version_is_dit(version) ? 16 : 4;
-            if (!use_tiny_autoencoder) {
-                C *= 2;
+            int NE2, NE3;
+            if (sd_version_is_qwen_image(version)) {
+                NE2 = x->ne[3];
+                NE3 = C;
             }
-            result = ggml_new_tensor_4d(work_ctx, GGML_TYPE_F32, W, H, C, x->ne[3]);
+            else {
+                if (!use_tiny_autoencoder) {
+                    C *= 2;
+                }
+                NE2 = C;
+                NE3 = x->ne[3];
+            }
+            result = ggml_new_tensor_4d(work_ctx, GGML_TYPE_F32, W, H, NE2, NE3);
         }
 
         if (sd_version_is_qwen_image(version)) {
