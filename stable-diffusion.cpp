7c3dd7e6ca86a4fb7427745c43f570ba1d9f428e
rmatif <rmatif@proton.me>
2025-12-10 20:07:04 +0000

    use actual scheduler sigmas for ucache bounds

--- stable-diffusion.cpp
+++ stable-diffusion.cpp
@@ -1558,6 +1558,10 @@ public:
             }
         }
 
+        if (ucache_enabled) {
+            ucache_state.set_sigmas(sigmas);
+        }
+
         size_t steps          = sigmas.size() - 1;
         struct ggml_tensor* x = ggml_dup_tensor(work_ctx, init_latent);
         copy_ggml_tensor(x, init_latent);
