1e855c84ee9fc3625b32df008c989a1e1c82f97c
Stéphane du Hamel <stephduh@live.fr>
2025-12-12 02:39:09 +0100

    patch size consistent with Flux1

--- stable-diffusion.cpp
+++ stable-diffusion.cpp
@@ -1478,12 +1478,6 @@ public:
                     latent_rgb_bias = flux2_latent_rgb_bias;
                     patch_sz               = 2;
                 }
-            } else if (dim == 64) {
-                if (sd_version_is_flux(version) || sd_version_is_z_image(version) || sd_version_is_longcat(version)) {
-                    latent_rgb_proj = flux_latent_rgb_proj;
-                    latent_rgb_bias = flux_latent_rgb_bias;
-                    patch_sz        = 2;
-                }
             } else if (dim == 48) {
                 if (sd_version_is_wan(version)) {
                     latent_rgb_proj = wan_22_latent_rgb_proj;
@@ -2270,7 +2264,7 @@ public:
         int vae_scale_factor = 8;
         if (version == VERSION_WAN2_2_TI2V) {
             vae_scale_factor = 16;
-        } else if (sd_version_is_flux2(version) || sd_version_is_longcat(version)) {
+        } else if (sd_version_is_flux2(version)) {
             vae_scale_factor = 16;
         } else if (version == VERSION_CHROMA_RADIANCE) {
             vae_scale_factor = 1;
@@ -2299,8 +2293,6 @@ public:
                 latent_channel = 3;
             } else if (sd_version_is_flux2(version)) {
                 latent_channel = 128;
-            } else if (sd_version_is_longcat(version)) {
-                latent_channel = 64;
             } else {
                 latent_channel = 16;
             }
@@ -2601,7 +2593,6 @@ public:
             sd_version_is_qwen_image(version) ||
             sd_version_is_wan(version) ||
             sd_version_is_flux2(version) ||
-            sd_version_is_longcat(version) ||
             version == VERSION_CHROMA_RADIANCE) {
             latent = vae_output;
         } else if (version == VERSION_SD1_PIX2PIX) {
