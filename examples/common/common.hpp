f120852cf7964b61e90e99504c3a1021a4020f68
piallai <59734081+piallai@users.noreply.github.com>
2026-01-02 12:14:26 +0100

    special case for -h/--help : not an error

--- examples/common/common.hpp
+++ examples/common/common.hpp
@@ -413,7 +413,11 @@ static bool parse_options(int argc, const char** argv, const std::vector<ArgOpti
                 break;
         }
 
-        if (invalid_arg && arg != "-h") {
+        if (arg == "-h" || arg == "--help") {
+            return false;
+        }
+
+        if (invalid_arg) {
             LOG_ERROR("error: invalid parameter for argument: %s", arg.c_str());
             return false;
         }
