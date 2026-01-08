0e52afc6513cc2dea9a1a017afc4a008d5acf2b0
rmatif <rmatif@proton.me>
2026-01-08 16:23:05 +0100

    feat: enable vae tiling for vid gen (#1152)
    * enable vae tiling for vid gen
    
    * format code
    
    * eliminate compilation warning
    
    ---------
    
    Co-authored-by: leejet <leejet714@gmail.com>

--- stable-diffusion.h
+++ stable-diffusion.h
@@ -319,6 +319,7 @@ typedef struct {
     int64_t seed;
     int video_frames;
     float vace_strength;
+    sd_tiling_params_t vae_tiling_params;
     sd_cache_params_t cache;
 } sd_vid_gen_params_t;
 
