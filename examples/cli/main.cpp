1183474269c0c49b5c01ad4d09113a21beeb1f62
Wagner Bruna <wbruna@yahoo.com>
2026-01-17 07:25:07 -0300

    fix: avoid undefined behavior on image mask allocation failure

--- examples/cli/main.cpp
+++ examples/cli/main.cpp
@@ -594,12 +594,12 @@ int main(int argc, const char* argv[]) {
         }
     } else {
         mask_image.data = (uint8_t*)malloc(gen_params.width * gen_params.height);
-        memset(mask_image.data, 255, gen_params.width * gen_params.height);
         if (mask_image.data == nullptr) {
             LOG_ERROR("malloc mask image failed");
             release_all_resources();
             return 1;
         }
+        memset(mask_image.data, 255, gen_params.width * gen_params.height);
     }
 
     if (gen_params.control_image_path.size() > 0) {
