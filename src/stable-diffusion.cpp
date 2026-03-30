559062d5b20ddd82f1fb320d22bef157d49109cd
leejet <leejet714@gmail.com>
2026-03-30 22:41:32 +0800

    fix: disable extra T5 mask padding for Wan

--- src/stable-diffusion.cpp
+++ src/stable-diffusion.cpp
@@ -493,7 +493,7 @@ public:
                                                                     offload_params_to_cpu,
                                                                     tensor_storage_map,
                                                                     true,
-                                                                    1,
+                                                                    0,
                                                                     true);
                 diffusion_model  = std::make_shared<WanModel>(backend,
                                                              offload_params_to_cpu,
