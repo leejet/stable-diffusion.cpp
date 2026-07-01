e17bb9a8a3b29140394ff63a56425183fbe8af33
leejet <leejet714@gmail.com>
2026-07-01 23:13:13 +0800

    fix windows ci

--- src/core/ggml_extend_backend.cpp
+++ src/core/ggml_extend_backend.cpp
@@ -365,6 +365,23 @@ bool sd_backend_cpu_set_n_threads(ggml_backend_t backend, int n_threads) {
     return false;
 }
 
+static ggml_cgraph sd_ggml_graph_view(ggml_cgraph* cgraph0, int i0, int i1) {
+    ggml_cgraph cgraph = {
+        /*.size             =*/0,
+        /*.n_nodes          =*/i1 - i0,
+        /*.n_leafs          =*/0,
+        /*.nodes            =*/cgraph0->nodes + i0,
+        /*.grads            =*/nullptr,
+        /*.grad_accs        =*/nullptr,
+        /*.leafs            =*/nullptr,
+        /*.use_counts       =*/cgraph0->use_counts,
+        /*.visited_hash_set =*/cgraph0->visited_hash_set,
+        /*.order            =*/cgraph0->order,
+        /*.uid              =*/0,
+    };
+    return cgraph;
+}
+
 ggml_status sd_backend_graph_compute_with_eval_callback(ggml_backend_t backend,
                                                         ggml_cgraph* gf,
                                                         sd_graph_eval_callback_t callback_eval,
@@ -387,7 +404,7 @@ ggml_status sd_backend_graph_compute_with_eval_callback(ggml_backend_t backend,
             need = callback_eval(t, true, callback_eval_user_data);
         }
 
-        ggml_cgraph gv = ggml_graph_view(gf, j0, j1 + 1);
+        ggml_cgraph gv = sd_ggml_graph_view(gf, j0, j1 + 1);
         status         = ggml_backend_graph_compute_async(backend, &gv);
         if (status != GGML_STATUS_SUCCESS) {
             break;
